471e6f8f6714693cfacf702ca4a33c6e2ea62682
GraceMwende <grace.micheni@student.moringaschool.com>
2022-02-07 09:43:18 +0300

    able to upload profile pic

--- app/__init__.py
+++ app/__init__.py
@@ -3,9 +3,11 @@ from flask_bootstrap import Bootstrap
 from config import config_options
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_uploads import UploadSet, configure_uploads,IMAGES
 
 bootstrap = Bootstrap()
 db = SQLAlchemy()
+photos = UploadSet('photos',IMAGES)
 
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
@@ -18,6 +20,9 @@ def create_app(config_name):
   # creating app configurations
   app.config.from_object(config_options[config_name])
   # app.config.from_pyfile('config.py')
+
+  #configure Uploadset
+  configure_uploads(app,photos)
 
   # initializing flask extensions
   bootstrap.init_app(app)
--- config.py
+++ config.py
@@ -4,6 +4,7 @@ class Config:
   SECRET_KEY = os.environ.get('SECRET_KEY')
   SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Access@localhost/pitch'
   SQLALCHEMY_TRACK_MODIFICATIONS = False
+  UPLOADED_PHOTOS_DEST = 'app/static/photos'
 
 class ProdConfig(Config):
   """Production configuration child class
