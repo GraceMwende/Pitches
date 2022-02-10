e111a1bb48b560b6b83b64bfa66482b6861b0c2b
GraceMwende <grace.micheni@student.moringaschool.com>
2022-02-10 10:21:07 +0300

    add pyscog binary

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 class Config:
   """General configuration parent class"""
   SECRET_KEY = os.environ.get('SECRET_KEY')
-  SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Access@localhost:5433/pitch'
+  SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Access@localhost/pitch'
   SQLALCHEMY_TRACK_MODIFICATIONS = False
   UPLOADED_PHOTOS_DEST = 'app/static/photos'
 
